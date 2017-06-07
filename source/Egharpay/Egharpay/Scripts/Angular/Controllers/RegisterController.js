5c8def097dedab188b44a4c0c9aa62efc0f17277
sunjayp888 <sunjayp88@gmail.com>
2017-06-08 00:20:25 +0530

    popup

---
+++ source/Egharpay/Egharpay/Scripts/Angular/Controllers/RegisterController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Egharpay')
+        .controller('RegisterController', HomeController);
+
+    HomeController.$inject = ['$window', 'Paging', 'OrderService', 'OrderBy', 'Order', '$uibModal'];
+
+    function RegisterController($window, Paging, OrderService, OrderBy, Order, $uibModal, $modalInstance) {
+        /* jshint validthis:true */
+        var vm = this;
+       
+
+        initialise();
+
+        function initialise() {
+            retrieveStatistics();
+            retrieveBarGraphStatistics();
+            retrieveCentres();
+        }
+
+        
+    }
+
+})();
