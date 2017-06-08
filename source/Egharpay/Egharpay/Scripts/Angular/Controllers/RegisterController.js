3a0bb422991eed88610a228884a53b8a1ada43b9
sunjayp888 <sunjayp88@gmail.com>
2017-06-08 08:45:03 +0530

    Added template

--- source/Egharpay/Egharpay/Scripts/Angular/Controllers/RegisterController.js
+++ source/Egharpay/Egharpay/Scripts/Angular/Controllers/RegisterController.js
@@ -3,24 +3,62 @@
 
     angular
         .module('Egharpay')
-        .controller('RegisterController', HomeController);
+        .controller('RegisterController', RegisterController);
 
-    HomeController.$inject = ['$window', 'Paging', 'OrderService', 'OrderBy', 'Order', '$uibModal'];
+    RegisterController.$inject = ['$window', 'Paging', 'OrderService', 'OrderBy', 'Order', '$uibModal'];
 
     function RegisterController($window, Paging, OrderService, OrderBy, Order, $uibModal, $modalInstance) {
         /* jshint validthis:true */
         var vm = this;
-       
+        vm.openPincodeModal = openPincodeModal
 
-        initialise();
+        vm.modalInstance = $uibModal.open({
+            size: 'md',
+            templateUrl: '/Scripts/Angular/Templates/PincodeModal.html',
+            controller: ['parent', '$uibModalInstance', function (parent, $uibModalInstance) {
+                var $modal = this;
+                $modal.parent = parent;
+                $modal.modalClose = modalClose;
+                $modal.modalSubmit = modalSubmit;
+                $modal.errorMessage = null;
 
-        function initialise() {
-            retrieveStatistics();
-            retrieveBarGraphStatistics();
-            retrieveCentres();
-        }
 
-        
+                function modalClose() { $uibModalInstance.dismiss(); }
+                function modalSubmit() {
+                    var isNew = !$modal.Holiday.PublicHolidayId;
+                    if (isNew) addNewHoliday($modal.Holiday);
+                    else updateHoliday($modal.Holiday);
+                }
+
+                function addNewHoliday(holiday) {
+                    $modal.parent.createHolidayToDB(holiday)
+                    .then(
+                        function (response) {
+                            if (!response.data.Succeeded)
+                                $modal.errorMessage = response.data.Message;
+                            else {
+                                if ($modal.parent.yearFilter != holiday.Date.getFullYear()) {
+                                    $modal.parent.yearFilter = holiday.Date.getFullYear();
+                                    $modal.parent.getAvailableYears();
+                                }
+                                else
+                                    $modal.parent.searchHolidays();
+
+                                $uibModalInstance.close(holiday);
+                            }
+                        },
+                        function (response) {
+                            $modal.errorMessage = 'There was a problem while trying to save the record.';
+                        });
+                }
+
+
+            }],
+            controllerAs: 'model',
+            resolve: {
+                parent: function () { return vm; },
+            }
+        });
     }
 
 })();
